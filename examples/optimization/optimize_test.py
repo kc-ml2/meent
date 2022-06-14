7f67c87b8f5fc10999d1d5514fd28cb11a8399b9
yonghakim <yonghakim.t@gmail.com>
2022-06-14 17:38:09 +0900

    JAX has problem with np.linalg.eig() with degeneracy.

--- examples/optimization/optimize_test.py
+++ examples/optimization/optimize_test.py
@@ -1,4 +1,6 @@
+from jax import grad
 import numpy as np
+import jax.numpy as jnp
 
 from meent.rcwa import RCWA
 
@@ -16,9 +18,32 @@ class RCWAOptimizer:
         residue = spectrum_model - spectrum_gt
         loss = np.linalg.norm(residue)
 
+class TT:
+    def __init__(self):
+        pass
+
+
 
 if __name__ == '__main__':
     grating_type = 0
+    aa = TT()
+    print(1)
+    gt = LalanneBase(grating_type,wls=np.linspace(0.5, 2.3, 40), fourier_order=1,)
+    # model = LalanneBase(grating_type, wls=np.linspace(0.5, 2.3, 40), fourier_order=1, thickness=[x])
+
+
+    def loss(x):
+        print(2)
+        model = LalanneBase(grating_type, wls=np.linspace(0.5, 2.3, 40), fourier_order=1, thickness=[x])
+        print(3)
+        model.lalanne_1d()
+
+        gap = jnp.linalg.norm(model.spectrum_r - gt.spectrum_r)
+        return gap
+
+    grad_loss = grad(loss)
+    print(grad_loss(1.0))
+
 
     gt = RCWA(grating_type)
     model = RCWA(grating_type)
