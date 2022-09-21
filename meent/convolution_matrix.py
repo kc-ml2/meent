42c59ad51c4ab2fbbc74e442c36faee82ed2977f
yonghakim <yonghakim.t@gmail.com>
2022-09-21 21:16:26 +0900

    adding pattern unit cell and conv_mat_3D

--- meent/convolution_matrix.py
+++ meent/convolution_matrix.py
@@ -7,59 +7,65 @@ from scipy.linalg import circulant
 from pathlib import Path
 
 
-def permittivity_mapping(pattern_all, wl, period, fourier_order, oneover=False):
-    # TODO: not fully implemented
-
-    # nk_pool = {'SILICON': np.array([[100, 400, 500, 600, 900], [3.48, 3.48, 3.48, 3.48, 3.48]])}
-    # TODO: take out
-    # TODO: pass to function layer by layer
-    nk_path = str(Path(__file__).resolve().parent.parent) + '/nk_data/p_Si.mat'
-    mat_si = scipy.io.loadmat(nk_path)
-
-    materials = {}
-    materials['SILICON'] = mat_si
+def put_n_ridge_in_pattern(pattern_all, wl, oneover=False):
+    # nk_path = str(Path(__file__).resolve().parent.parent) + '/nk_data/p_Si.mat'
+    # mat_si = scipy.io.loadmat(nk_path)
+    #
+    # materials = {}
+    # materials['SILICON'] = mat_si
 
     pattern_all = copy.deepcopy(pattern_all)
 
     for i, (n_ridge, n_groove, pattern) in enumerate(pattern_all):
 
         if type(n_ridge) == str:
-            mat_property = materials[n_ridge.upper()]
-            n_ridge = np.interp(wl, mat_property['WL'].flatten(), mat_property['n'].flatten())
-
+            material = n_ridge
+            n_ridge = find_n_index(material, wl)
         pattern_all[i][0] = n_ridge if not oneover else 1 / n_ridge
+    return pattern_all
 
+
+def find_n_index(material, wl):
+    # TODO: where put this to?
+    nk_path = str(Path(__file__).resolve().parent.parent) + '/nk_data/p_Si.mat'
+    mat_si = scipy.io.loadmat(nk_path)
+
+    mat_table = {}
+    mat_table['SILICON'] = mat_si
+
+    mat_property = mat_table[material.upper()]
+    n_index = np.interp(wl, mat_property['WL'].flatten(), mat_property['n'].flatten())
+
+    return n_index
+
+
+def permittivity_mapping_by_fill_factor(pattern_all, wl, period, fourier_order, oneover=False):
+    pattern_all = put_n_ridge_in_pattern(pattern_all, wl, oneover)
     if len(period) == 1:
-        if type(pattern_all[0][2]) in [float, int]:  # TODO: better condition
-            pmtvy = draw_1d_fill_factor(pattern_all)
-        else:
-            pmtvy = draw_1d_jlab(pattern_all)
-
+        pmtvy = draw_1d_fill_factor(pattern_all)
     else:
-        pmtvy = draw_2d(pattern_all)
+        pmtvy = draw_2d_fill_factor(pattern_all)
 
-    conv_all = to_conv_mat(pmtvy, fourier_order)
+    conv_all = to_conv_mat_fill_factor(pmtvy, fourier_order)
 
     return conv_all
 
 
-def to_conv_mat(pmt, fourier_order):
-    # FFT scaling
-    # https://kr.mathworks.com/matlabcentral/answers/15770-scaling-the-fft-and-the-ifft#:~:text=the%20matlab%20fft%20outputs%202,point%20is%20the%20parseval%20equation.
+def to_conv_mat_fill_factor(pmt, fourier_order):
+    # FFT scaling: https://kr.mathworks.com/matlabcentral/answers/15770-scaling-the-fft-and-the-ifft?s_tid=srchtitle
     ff = 2 * fourier_order + 1
 
     # TODO: check whether 1D case is correct or not. I think I actually didn't test it at all.
     if len(pmt[0].shape) == 1:  # 1D
         res = np.ndarray((len(pmt), 2*fourier_order+1, 2*fourier_order+1)).astype('complex')
 
-        # extend array
+        # extend array for FFT
         if pmt.shape[1] < 2 * ff + 1:
             n = (2 * ff + 1) // pmt.shape[1]
             pmt = np.repeat(pmt, n+1, axis=1)
 
         for i, pmtvy in enumerate(pmt):
-            pmtvy_fft = np.fft.fftn(pmtvy / pmtvy.size)
-            pmtvy_fft = np.fft.fftshift(pmtvy_fft)
+            pmtvy_fft = np.fft.fftshift(np.fft.fftn(pmtvy / pmtvy.size))
 
             center = len(pmtvy_fft) // 2
             pmtvy_fft_cut = (pmtvy_fft[-ff + center: center+ff+1])
@@ -73,16 +79,15 @@ def to_conv_mat(pmt, fourier_order):
 
         # extend array
         # TODO: run test
-        if pmt.shape[0] < 2 * ff + 1:
-            n = (2 * ff + 1) // pmt.shape[1]
-            pmt = np.repeat(pmt, n+1, axis=0)
         if pmt.shape[1] < 2 * ff + 1:
-            n = (2 * ff + 1) // pmt.shape[1]
+            n = (2 * ff + 1) // pmt.shape[1]  # TODO: shape[1]? or [0]?
             pmt = np.repeat(pmt, n+1, axis=1)
+        if pmt.shape[2] < 2 * ff + 1:
+            n = (2 * ff + 1) // pmt.shape[2]
+            pmt = np.repeat(pmt, n+1, axis=2)
 
         for i, layer in enumerate(pmt):
-            pmtvy_fft = np.fft.fftn(layer / layer.size)
-            pmtvy_fft = np.fft.fftshift(pmtvy_fft)
+            pmtvy_fft = np.fft.fftshift(np.fft.fft2(layer / layer.size))
 
             center = np.array(pmtvy_fft.shape) // 2
 
@@ -104,6 +109,40 @@ def to_conv_mat(pmt, fourier_order):
     return res
 
 
+def to_conv_mat_3d(cell, fourier_order):
+    # attention on the order of axis.
+    # TODO: Here X Y Z. Cell Drawing in CAD is Y X Z.
+    ff = 2 * fourier_order + 1
+
+    # TODO: separate fourier order
+    res = np.ndarray((cell.shape[-1], ff ** 2, ff ** 2)).astype('complex')
+
+    # extend array
+    # TODO: run test
+    if cell.shape[0] < 2 * ff + 1:
+        n = (2 * ff + 1) // cell.shape[0]
+        cell = np.repeat(cell, n + 1, axis=0)
+    if cell.shape[1] < 2 * ff + 1:
+        n = (2 * ff + 1) // cell.shape[1]
+        cell = np.repeat(cell, n + 1, axis=1)
+
+    for i in range(cell.shape[-1]):
+        layer = cell[:, :, i]
+        pmtvy_fft = np.fft.fftshift(np.fft.fft2(layer / layer.size))
+
+        center = np.array(pmtvy_fft.shape) // 2
+
+        conv_idx = np.arange(ff - 1, -ff, -1)
+        conv_idx = circulant(conv_idx)[ff - 1:, :ff]
+
+        conv_i = np.repeat(conv_idx, ff, axis=1)
+        conv_i = np.repeat(conv_i, [ff] * ff, axis=0)
+        conv_j = np.tile(conv_idx, (ff, ff))
+        res[i] = pmtvy_fft[center[0] + conv_i, center[1] + conv_j]
+
+    return res
+
+
 def draw_1d_fill_factor(patterns, resolution=1001):
     # fill_factor is not exactly implemented.
     res = np.ndarray((len(patterns), resolution))
@@ -119,47 +158,22 @@ def draw_1d_fill_factor(patterns, resolution=1001):
     return res
 
 
-def draw_1d_array(patterns_pixel, resolution=1001):  # TODO: Implement
-    #
-    # resolution = len(patterns_pixel[0][2].flatten())
-    # res = np.ndarray((len(patterns_pixel), resolution))
-    #
-    # for i, (n_ridge, n_groove, pixel_map) in enumerate(patterns_pixel):
-    #     pixel_map = np.array(pixel_map, dtype='float')
-    #     # permittivity = np.ones(resolution) * n_groove
-    #     pixel_map = (pixel_map + 1) / 2
-    #     pixel_map = pixel_map * (n_ridge**2 - n_groove**2) + n_groove ** 2
-    #     res[i] = pixel_map
-    #
-    # return res
-    pass
-
-
-def draw_1d_jlab(patterns_pixel, resolution=1001):
-
-    resolution = len(patterns_pixel[0][2].flatten())
-    res = np.ndarray((len(patterns_pixel), resolution))
-
-    for i, (n_ridge, n_groove, pixel_map) in enumerate(patterns_pixel):
-        pixel_map = np.array(pixel_map, dtype='float')
-        pixel_map = (pixel_map + 1) / 2
-        pixel_map = pixel_map * (n_ridge**2 - n_groove**2) + n_groove ** 2
-        res[i] = pixel_map
+def draw_2d(patterns, resolution=1001):
+    # TODO: Implement
+    res = np.ndarray((len(patterns), resolution, resolution))
 
     return res
 
 
-def draw_2d(patterns, resolution=1001):
-
-    # TODO: Implement
+def draw_2d_fill_factor(patterns, resolution=1001):
     res = np.ndarray((len(patterns), resolution, resolution))
 
     for i, (n_ridge, n_groove, fill_factor) in enumerate(patterns):
-
+        fill_factor = np.array(fill_factor).reshape(-1)  # TODO: handle outside?
         permittivity = np.ones((resolution, resolution))
-        cut = int(resolution * fill_factor)
-        permittivity[:, :] *= n_groove ** 2
-        permittivity[:, :cut] *= n_ridge ** 2
+        cut = (resolution * fill_factor)
+        permittivity *= n_groove ** 2
+        permittivity[:int(cut[-1]), :int(cut[0])] *= n_ridge ** 2
         res[i] = permittivity
 
     return res
--- meent/rcwa.py
+++ meent/rcwa.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 
 from meent._base import _BaseRCWA
+from meent.convolution_matrix import to_conv_mat_3d, find_n_index
 
 
 class RCWA(_BaseRCWA):
@@ -11,20 +12,34 @@ class RCWA(_BaseRCWA):
         super().__init__(grating_type, n_I, n_II, theta, phi, psi, fourier_order, period, wls, pol, patterns,
                          thickness, algo)
         self.spectrum_r, self.spectrum_t = None, None
-        self._init_spectrum()
+        self.init_spectrum_array()
 
-    def _init_spectrum(self):
-        if self.grating_type in (0, 1):
-            self.spectrum_r = np.ndarray((len(self.wls), 2 * self.fourier_order + 1))
-            self.spectrum_t = np.ndarray((len(self.wls), 2 * self.fourier_order + 1))
-        elif self.grating_type == 2:
-            self.spectrum_r = np.ndarray((len(self.wls), self.ff, self.ff))
-            self.spectrum_t = np.ndarray((len(self.wls), self.ff, self.ff))
-        else:
-            raise ValueError
+    # def _init_spectrum(self):
+    #     if self.grating_type in (0, 1):
+    #         self.spectrum_r = np.ndarray((len(self.wls), 2 * self.fourier_order + 1))
+    #         self.spectrum_t = np.ndarray((len(self.wls), 2 * self.fourier_order + 1))
+    #     elif self.grating_type == 2:
+    #         self.spectrum_r = np.ndarray((len(self.wls), self.ff, self.ff))
+    #         self.spectrum_t = np.ndarray((len(self.wls), self.ff, self.ff))
+    #     else:
+    #         raise ValueError
 
-    def run_single(self, wl):
-        E_conv_all, oneover_E_conv_all = self.get_permittivity_map(wl)
+    # def run_single(self, wl):
+    #     E_conv_all, oneover_E_conv_all = self.get_permittivity_map(wl)
+    #
+    #     if self.grating_type == 0:
+    #         de_ri, de_ti = self.solve_1d(wl, E_conv_all, oneover_E_conv_all)
+    #     elif self.grating_type == 1:
+    #         # de_ri, de_ti = self.solve_1d_conical()  # TODO: implement
+    #         de_ri = de_ti = None
+    #     elif self.grating_type == 2:
+    #         de_ri, de_ti = self.solve_2d(wl, E_conv_all, oneover_E_conv_all)
+    #     else:
+    #         raise ValueError
+    #
+    #     return de_ri, de_ti
+
+    def solve(self, wl, E_conv_all, oneover_E_conv_all):
 
         if self.grating_type == 0:
             de_ri, de_ti = self.solve_1d(wl, E_conv_all, oneover_E_conv_all)
@@ -38,15 +53,46 @@ class RCWA(_BaseRCWA):
 
         return de_ri, de_ti
 
-    def loop_wavelength(self, wavelength_array=None):
+    def loop_wavelength_fill_factor(self, wavelength_array=None):
 
         if wavelength_array is not None:
             self.wls = wavelength_array
-            self._init_spectrum()
+            self.init_spectrum_array()
 
         for i, wl in enumerate(self.wls):
+            E_conv_all, oneover_E_conv_all = self.get_e_conv_set_by_fill_factor(wl)
 
-            de_ri, de_ti = self.run_single(wl)
+            de_ri, de_ti = self.solve(wl, E_conv_all, oneover_E_conv_all)
+
+            self.spectrum_r[i] = de_ri
+            self.spectrum_t[i] = de_ti
+
+        return self.spectrum_r, self.spectrum_t
+
+    def loop_wavelength_ucell(self):
+
+        cell = np.zeros((4, 4, 4))
+
+        # si = [[0, 4], [0, 4], [0, 1]]
+        # oxide = [[0, 4], [0, 4], [1, 2]]
+        # ni = [[0, 4], [0, 4], [2, 3]]
+        # ti = [[0, 4], [0, 4], [3, 4]]
+
+        # si = [[x_begin, x_end], [y_begin, y_end], [z_begin, z_end]]
+        si = ['SILICON', 0, 4, 0, 4, 0, 1]
+        oxide = ['SILICON', 0, 4, 0, 4, 1, 2]
+        ni = ['SILICON', 0, 4, 0, 4, 2, 3]
+        ti = ['SILICON', 0, 4, 0, 4, 3, 4]
+
+        for i, wl in enumerate(self.wls):
+            for material, x_begin, x_end, y_begin, y_end, z_begin, z_end in [si, oxide, ni, ti]:
+                n_index = find_n_index(material, wl)
+                cell[x_begin:x_end, y_begin:y_end, z_begin:z_end] = n_index ** 2
+
+            e_conv_all = to_conv_mat_3d(cell, fourier_order)
+            oneover_e_conv_all = to_conv_mat_3d(1 / cell, fourier_order)
+
+            de_ri, de_ti = self.solve(wl, e_conv_all, oneover_e_conv_all)
 
             self.spectrum_r[i] = de_ri
             self.spectrum_t[i] = de_ti
@@ -55,36 +101,41 @@ class RCWA(_BaseRCWA):
 
 
 if __name__ == '__main__':
-    grating_type = 0
+    grating_type = 2
     pol = 0
 
-    n_I = 1.45  # glass
+    n_I = 1
     n_II = 1
 
     theta = 0
     phi = 0
     psi = 0 if pol else 90
 
-    wls = np.linspace(500, 2300, 10)
+    wls = np.linspace(500, 2300, 100)
 
-    if grating_type == 2:
-        period = [700, 700]
-        fourier_order = 2
-
-    else:
+    if grating_type == 0:
         period = [700]
-        fourier_order = 10
+        patterns = [[3.48, 1, 0.3], [3.48, 1, 0.3]]  # n_ridge, n_groove, fill_factor
+        fourier_order = 40
 
-    # permittivity in grating layer
-    patterns = [[3.48, 1, 0.3], [3.48, 1, 0.3]]  # n_ridge, n_groove, fill_factor
+    elif grating_type == 2:
+        period = [700, 700]
+        patterns = [[3.48, 1, [0.3, 1]], [3.48, 1, [0.3, 1]]]  # n_ridge, n_groove, fill_factor
+        fourier_order = 2
+    else:
+        raise ValueError
+
     thickness = [460, 660]
 
     AA = RCWA(grating_type=grating_type, pol=pol, n_I=n_I, n_II=n_II, theta=theta, phi=phi, psi=psi,
               fourier_order=fourier_order, wls=wls, period=period, patterns=patterns, thickness=thickness)
     t0 = time.perf_counter()
 
-    a, b = AA.loop_wavelength()
+    a, b = AA.loop_wavelength_fill_factor()
     AA.plot()
 
     print(time.perf_counter() - t0)
 
+    AA.loop_wavelength_ucell()
+    AA.plot()
+    print(1)
