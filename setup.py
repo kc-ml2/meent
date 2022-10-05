bb6c47e2eb6cdc84f1205ba69436ebf53aec922e
yonghakim <yonghakim.t@gmail.com>
2022-10-05 20:26:41 +0900

    adding github workflow

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meent',
+    version='0.3.3',
     url='https://github.com/kc-ml2/meent',
     author='KC ML2',
     author_email='yongha@kc-ml2.com',
