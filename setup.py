6b1e4406f20f41cbfd12e909296de019746f1e21
yonghakim <yonghakim.t@gmail.com>
2023-02-07 00:35:14 +0900

    v0.7.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meent',
-    version='0.7.0',
+    version='0.7.1',
     url='https://github.com/kc-ml2/meent',
     author='KC ML2',
     author_email='yongha@kc-ml2.com',
