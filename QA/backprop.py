9e4a5e2b6a09624a9ab4df32d35844bba7cc7d9c
yonghakim <yonghakim.t@gmail.com>
2023-02-23 23:25:43 +0900

    turn on jit on self.solve; change backprop.py to include multilayer ucell;

--- QA/backprop.py
+++ QA/backprop.py
@@ -9,6 +9,7 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import time
+from copy import deepcopy
 
 from meent.rcwa import call_solver
 import torch
@@ -31,17 +32,35 @@ def load_setting(mode_key, dtype, device):
     ucell_materials = [1, 3.48]
     fourier_order = 2
 
-    thickness, period = [1120.], [1000, 1000]
-
+    period = [1000, 1000]
+    thickness = [1120., 400, 300]
     ucell = np.array(
-        [[
-            [3.1, 1.1, 1.2, 1.6, 3.1],
-            [3.5, 1.4, 1.1, 1.2, 3.6],
-            [3.1, 1.2, 1.5, 1.1, 3.1],
-            [3.5, 1.2, 1.5, 1.2, 3.3],
-            [3.1, 1.5, 1.5, 1.4, 3.1],
-        ]]
+        [
+            [
+                [3.1, 1.1, 1.2, 1.6, 3.1],
+                [3.5, 1.4, 1.1, 1.2, 3.6],
+            ],
+            [
+                [3.5, 1.2, 1.5, 1.2, 3.3],
+                [3.1, 1.5, 1.5, 1.4, 3.1],
+            ],
+            [
+                [3.5, 1.2, 1.5, 1.2, 3.3],
+                [3.1, 1.5, 1.5, 1.4, 3.1],
+            ],
+        ]
     )
+
+    # thickness, period = [1120.], [1000, 1000]
+    #
+    # ucell = np.array(
+    #     [
+    #         [
+    #             [3.5, 1.2, 1.5, 1.2, 3.3],
+    #             [3.1, 1.5, 1.5, 1.4, 3.1],
+    #         ],
+    #     ]
+    # )
 
     if mode_key == 0:
         device = 0
@@ -50,6 +69,9 @@ def load_setting(mode_key, dtype, device):
 
     elif mode_key == 1:  # JAX
         jax.config.update('jax_platform_name', 'cpu') if device == 0 else jax.config.update('jax_platform_name', 'gpu')
+
+        thickness = jnp.array(thickness)
+        period = jnp.array(period)
 
         if dtype == 0:
             jax.config.update("jax_enable_x64", True)
@@ -65,6 +87,9 @@ def load_setting(mode_key, dtype, device):
     else:  # Torch
         device = torch.device('cpu') if device == 0 else torch.device('cuda')
         type_complex = torch.complex128 if dtype == 0 else torch.complex64
+
+        thickness = torch.tensor(thickness)
+        period = torch.tensor(period)
 
         if dtype == 0:
             ucell = torch.tensor(ucell, dtype=torch.float64, device=device)
@@ -101,30 +126,29 @@ def optimize_jax_ucell_metasurface(mode_key, dtype, device):
     def grad_numerical(ucell, delta):
 
         grad_arr = np.zeros(ucell.shape)
+        for layer in range(ucell.shape[0]):
+            for r in range(ucell.shape[1]):
+                for c in range(ucell.shape[2]):
+                    ucell_delta_m = ucell.at[layer, r, c].set(ucell[layer, r, c] - delta)
 
-        for r in range(ucell.shape[1]):
-            for c in range(ucell.shape[2]):
+                    E_conv_all_m = to_conv_mat(ucell_delta_m, fourier_order, type_complex=type_complex)
+                    o_E_conv_all_m = to_conv_mat(1 / ucell_delta_m, fourier_order, type_complex=type_complex)
+                    de_ri_delta_m, de_ti_delta_m, _, _, _ = solver.solve(wavelength, E_conv_all_m, o_E_conv_all_m)
 
-                ucell_delta_m = ucell.at[0, r, c].set(ucell[0, r, c] - delta)
+                    ucell_delta_p = ucell.at[layer, r, c].set(ucell[layer, r, c] + delta)
 
-                E_conv_all = to_conv_mat(ucell_delta_m, fourier_order, type_complex=type_complex)
-                o_E_conv_all = to_conv_mat(1 / ucell_delta_m, fourier_order, type_complex=type_complex)
-                de_ri_delta_m, de_ti_delta_m, _, _, _ = solver.solve(wavelength, E_conv_all, o_E_conv_all)
+                    E_conv_all_p = to_conv_mat(ucell_delta_p, fourier_order, type_complex=type_complex)
+                    o_E_conv_all_p = to_conv_mat(1 / ucell_delta_p, fourier_order, type_complex=type_complex)
+                    de_ri_delta_p, de_ti_delta_p, _, _, _ = solver.solve(wavelength, E_conv_all_p, o_E_conv_all_p)
 
-                ucell_delta_p = ucell.at[0, r, c].set(ucell[0, r, c] + delta)
-
-                E_conv_all = to_conv_mat(ucell_delta_p, fourier_order, type_complex=type_complex)
-                o_E_conv_all = to_conv_mat(1 / ucell_delta_p, fourier_order, type_complex=type_complex)
-                de_ri_delta_p, de_ti_delta_p, _, _, _ = solver.solve(wavelength, E_conv_all, o_E_conv_all)
-
-                center = de_ti_delta_m.shape[0] // 2
-                grad_numeric = (de_ti_delta_p[center, center+1] - de_ti_delta_m[center, center+1]) / (2*delta)
-                grad_arr[0, r, c] = grad_numeric
+                    center = de_ti_delta_m.shape[0] // 2
+                    grad_numeric = (de_ti_delta_p[center, center+1] - de_ti_delta_m[center, center+1]) / (2*delta)
+                    grad_arr[layer, r, c] = grad_numeric
 
         return grad_arr
 
-    print('grad_ad:', grad_loss(ucell))
-    print('grad_numeric:', grad_numerical(ucell, 1E-5))
+    print('JAX grad_ad:\n', grad_loss(ucell))
+    print('JAX grad_numeric:\n', grad_numerical(ucell, 1E-6))
 
 
 def optimize_torch_metasurface(mode_key, dtype, device):
@@ -151,37 +175,36 @@ def optimize_torch_metasurface(mode_key, dtype, device):
     c = de_ti.shape[0] // 2
     loss = de_ti[c, c + 1]
     loss.backward()
-    print(ucell.grad)
+    print('Torch grad_ad:\n', ucell.grad)
 
     def grad_numerical(ucell, delta):
         ucell.requires_grad = False
         grad_arr = torch.zeros(ucell.shape)
 
-        for r in range(ucell.shape[1]):
-            for c in range(ucell.shape[2]):
+        for layer in range(ucell.shape[0]):
+            for r in range(ucell.shape[1]):
+                for c in range(ucell.shape[2]):
+                    ucell_delta_m = deepcopy(ucell)
+                    ucell_delta_m[layer, r, c] -= delta
 
-                from copy import deepcopy
-                ucell_delta_m = deepcopy(ucell)
-                ucell_delta_m[0, r, c] -= delta
+                    E_conv_all = to_conv_mat(ucell_delta_m, fourier_order, type_complex=type_complex)
+                    o_E_conv_all = to_conv_mat(1 / ucell_delta_m, fourier_order, type_complex=type_complex)
+                    de_ri_delta_m, de_ti_delta_m, _, _, _ = solver.solve(wavelength, E_conv_all, o_E_conv_all)
 
-                E_conv_all = to_conv_mat(ucell_delta_m, fourier_order, type_complex=type_complex)
-                o_E_conv_all = to_conv_mat(1 / ucell_delta_m, fourier_order, type_complex=type_complex)
-                de_ri_delta_m, de_ti_delta_m, _, _, _ = solver.solve(wavelength, E_conv_all, o_E_conv_all)
+                    ucell_delta_p = deepcopy(ucell)
+                    ucell_delta_p[layer, r, c] += delta
 
-                ucell_delta_p = deepcopy(ucell)
-                ucell_delta_p[0, r, c] += delta
+                    E_conv_all = to_conv_mat(ucell_delta_p, fourier_order, type_complex=type_complex)
+                    o_E_conv_all = to_conv_mat(1 / ucell_delta_p, fourier_order, type_complex=type_complex)
+                    de_ri_delta_p, de_ti_delta_p, _, _, _ = solver.solve(wavelength, E_conv_all, o_E_conv_all)
 
-                E_conv_all = to_conv_mat(ucell_delta_p, fourier_order, type_complex=type_complex)
-                o_E_conv_all = to_conv_mat(1 / ucell_delta_p, fourier_order, type_complex=type_complex)
-                de_ri_delta_p, de_ti_delta_p, _, _, _ = solver.solve(wavelength, E_conv_all, o_E_conv_all)
-
-                center = de_ti_delta_m.shape[0] // 2
-                grad_numeric = (de_ti_delta_p[center, center+1] - de_ti_delta_m[center, center+1]) / (2*delta)
-                grad_arr[0, r, c] = grad_numeric
+                    center = de_ti_delta_m.shape[0] // 2
+                    grad_numeric = (de_ti_delta_p[center, center+1] - de_ti_delta_m[center, center+1]) / (2*delta)
+                    grad_arr[layer, r, c] = grad_numeric
 
         return grad_arr
 
-    print('grad_numeric:', grad_numerical(ucell, 1E-5))
+    print('Torch grad_numeric:\n', grad_numerical(ucell, 1E-6))
 
 
 if __name__ == '__main__':
