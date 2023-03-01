47d81a1b31e940c6f0e75bbff09282f0a680d148
yonghakim <yonghakim.t@gmail.com>
2023-03-02 00:23:09 +0900

    patch version update

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='meent',
-    version='0.7.10',
+    version='0.7.11',
     url='https://github.com/kc-ml2/meent',
     author='KC ML2',
     author_email='yongha@kc-ml2.com',
     packages=['meent'] + find_packages(include=['meent.*']),
     install_requires=[
         'numpy==1.23.3',
-        'jax==0.4.1',
     ],
     python_requires='>=3.8',
     long_description_content_type="text/markdown",
