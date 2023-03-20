97c38c6c46fe8a6e09fb62cd74aed1f4b9f72071
yonghakim <yonghakim.t@gmail.com>
2023-03-21 00:12:07 +0900

    bugfix:eigendecomposition in JAX; newly implemented Eig in Torch;

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras = {
 }
 setup(
     name='meent',
-    version='0.8.4',
+    version='0.8.5',
     url='https://github.com/kc-ml2/meent',
     author='KC ML2',
     author_email='yongha@kc-ml2.com',
