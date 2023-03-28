66b4abdfa0e3575b429bab079536b8297c6c0811
yonghakim <yonghakim.t@gmail.com>
2023-03-29 00:32:26 +0900

    modify loss function in JAX;

--- meent/on_jax/optimizer/loss.py
+++ meent/on_jax/optimizer/loss.py
@@ -7,14 +7,20 @@ class LossDeflector:
         self.x_order = x_order
         self.y_order = y_order
 
-    def __call__(self, value, target=1, *args, **kwargs):
+    def __call__(self, value, *args, **kwargs):
         de_ri, de_ti = value
-        c_x = de_ti.shape[0] // 2
-        c_y = de_ti.shape[1] // 2
 
-        res = de_ti[c_x + self.x_order, c_y + self.y_order]
+        if len(de_ti.shape) == 1:
+            c_x = de_ti.shape[0] // 2
+            res = de_ti[c_x + self.x_order]
+        elif len(de_ti.shape) == 2:
+            c_x = de_ti.shape[0] // 2
+            c_y = de_ti.shape[1] // 2
+            res = de_ti[c_x + self.x_order, c_y + self.y_order]
+        else:
+            raise ValueError
 
-        return target - res
+        return res
 
 
 class LossSpectrumL2:
