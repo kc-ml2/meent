9a182553c36d2682ccfc0e716ca75a48cf091c56
yonghakim <yonghakim.t@gmail.com>
2023-09-06 13:43:59 +0900

    version update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras = {
 }
 setup(
     name='meent',
-    version='0.9.6',
+    version='0.9.7',
     url='https://github.com/kc-ml2/meent',
     author='KC ML2',
     author_email='yongha@kc-ml2.com',
