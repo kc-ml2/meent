214330839e9d9f390488655c032bcdfa31e3b3fc
yonghakim <yonghakim.t@gmail.com>
2024-06-11 00:09:49 +0900

    install read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,3 +49,12 @@ source_suffix = ['.rst', '.ipynb', '.md']
 # of the sidebar.
 html_logo = 'images/meent_logo.png'
 html_favicon = 'images/meent_logo2.png'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_css_files = [
+    'style.css',
+]
