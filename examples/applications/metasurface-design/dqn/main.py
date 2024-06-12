d628a54d96456454f9cb7a01e5008a859761cf6e
anthony0727 <jwseok0727@gmail.com>
2024-06-12 12:15:32 +0000

    apps: hugginface support

--- examples/applications/metasurface-design/dqn/main.py
+++ examples/applications/metasurface-design/dqn/main.py
@@ -56,19 +56,6 @@ class Callbacks(DefaultCallbacks):
     but when distributed training is used, it's inevitable
     """
 
-    def on_create_policy(self, *, policy_id, policy) -> None:
-        # state_dict = torch.load(
-        #     PRETRAINED_CKPT,
-        #     map_location=torch.device('cpu')
-        # )
-        # policy.set_weights(state_dict)
-        pass
-
-    def on_algorithm_init(self, *, algorithm, **kwargs) -> None:
-        # print(algorithm.get_policy().model)
-        # seed_all(42)
-        pass
-
     def _get_max(self, base_env):
         # retrieve `env.best`, where env is wrapped with BestWrapper to record the best structure
         bests = [e.best for e in base_env.get_sub_environments()]
@@ -111,15 +98,6 @@ if __name__ == '__main__':
         help='absolute path to data directory'
     )
     parser.add_argument(
-        '--transfer_ckpt', type=str, default=None,
-        help='absolute path to checkpoint file to do transfer learning'
-    )
-    parser.add_argument(
-        '--pretrained_ckpt', type=str,
-        default=f'/data1/EM-data/Jan31_normalized_UNet_256_2man_1100_60_0.004_0.008_stateDict.pt',
-        help='absolute path to checkpoint file of pretrained model'
-    )
-    parser.add_argument(
         '--wavelength', type=int, default=1100,
         help='wavelength of the incident light'
     )
@@ -139,6 +117,14 @@ if __name__ == '__main__':
         '--seed', type=int, default=42,
         help='random seed'
     )
+    parser.add_argument(
+        '--num_cpus_per_worker', type=int, default=4,
+        help='random seed'
+    )
+    parser.add_argument(
+        '--num_rollout_workers', type=int, default=8,
+        help='random seed'
+    )
 
     args = parser.parse_args()
 
@@ -152,7 +138,7 @@ if __name__ == '__main__':
 
     env_id = 'MeentIndex-v0'
     env_config = {'wavelength': args.wavelength, 'desired_angle': args.angle, 'thickness': args.thickness}#, 'seed': SEED}
-    model_cls = ShallowUQNet  # model_cls = ShallowUQNet / FCNQNet / FCNQNet_heavy
+    model_cls = ShallowUQNet
 
     def make_env(config):
         # config['seed'] = config['seed'] + config.worker_index
@@ -167,19 +153,15 @@ if __name__ == '__main__':
 
 
     from configs.simple_q import multiple_worker as config
-    # l = [(16, 1), (32, 1), (1, 1)]
-    # l = [(1, 1), (1, 2), (1, 4), (1, 8), (1, 16), (1, 32), (2, 1), (4, 1), (8, 1), (16, 1), (32, 1)]
-    # for i, j in l:
     ray.init(
         local_mode=False,# num_cpus=80, num_gpus=1, 
-        # include_dashboard=True, dashboard_host="0.0.0.0", dashboard_port=8266
     )
 
     register_env(env_id, lambda c: make_env(env_config))
     ModelCatalog.register_custom_model(model_cls.__name__, model_cls)
-    config.resources(num_cpus_per_worker=4, num_gpus=1)
-    config.rollouts(num_rollout_workers=8)
-    # from configs.simple_q import single_worker as config
+    config.resources(num_cpus_per_worker=args.num_cpus_per_worker, num_gpus=1)
+    config.rollouts(num_rollout_workers=args.num_rollout_workers)
+    
     config.policies = None
     config.framework(
         framework='torch'
@@ -197,59 +179,31 @@ if __name__ == '__main__':
         # seed=tune.grid_search([1, 2, 3, 4, 5]) # if you want to run experiments with multiple seeds
     )
 
-    if args.train:
-        algo = config.build()
-        print('*'*20, count_model_params(algo.get_policy().model))
-        if args.transfer_ckpt:
-            algo.load_checkpoint(args.transfer_ckpt)
-        stop = {
-            "timesteps_total": args.train_steps,
-        }
-        tuner = tune.Tuner(
-            'SimpleQ',
-            param_space=config.to_dict(),
-            # tune_config=tune.TuneConfig(), # for hparam search
-            run_config=air.RunConfig(
-                stop=stop,
-                local_dir=DATA_DIR,
-                # name=LOG_DIR,
-                checkpoint_config=air.CheckpointConfig(
-                    num_to_keep=5,
-                    checkpoint_score_attribute='episode_reward_max',
-                    checkpoint_score_order='max',
-                    checkpoint_frequency=50,
-                    checkpoint_at_end=True,
-                ),
+    algo = config.build()
+    print('*'*20, count_model_params(algo.get_policy().model))
+    
+    stop = {
+        "timesteps_total": args.train_steps,
+    }
+    tuner = tune.Tuner(
+        'SimpleQ',
+        param_space=config.to_dict(),
+        # tune_config=tune.TuneConfig(), # for hparam search
+        run_config=air.RunConfig(
+            stop=stop,
+            local_dir=DATA_DIR,
+            # name=LOG_DIR,
+            checkpoint_config=air.CheckpointConfig(
+                num_to_keep=5,
+                checkpoint_score_attribute='episode_reward_max',
+                checkpoint_score_order='max',
+                checkpoint_frequency=50,
+                checkpoint_at_end=True,
             ),
-        )
+        ),
+    )
 
-        results = tuner.fit()
-    else:
-        config.exploration(
-            explore=True,
-            exploration_config={
-                "type": "EpsilonGreedy",
-                'initial_epsilon': 0.01,
-                'final_epsilon': 0.01,
-                'epsilon_timesteps': 10000,
-            }
-        )
-        algo = config.build()
-        l = []
-        env = make_env(env_config)
-        algo.load_checkpoint('/home/anthony/physics-informed-metasurface/run/run/20240216_065402/SimpleQ_MeentIndex-v0_34abe_00000_0_2024-02-16_06-54-18/checkpoint_000196')
-        obs = env.reset(
-            # initial_struct=np.load('/home/anthony/physics-informed-metasurface/run/20240216_065402/w16_74-239999.npy')
-        )
-        
-        for i in tqdm(range(10000)):
-            ac = algo.compute_single_action(obs)
-            print(ac)
-            next_obs, rew, done, info = env.step(ac)
-            l.append((obs, env.eff))#(obs, rew))
-            print(env.eff)
-            obs = next_obs
+    results = tuner.fit()
 
-        np.save('test.npy', l)
 
 
